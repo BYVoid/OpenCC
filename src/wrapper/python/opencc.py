9215246b11c09faf6e0d9a1d167c3e533d323a5e
byvoid1 <devnull@localhost>
2010-06-18 23:01:33 +0800

    爲python封裝實現釋放內存，增加dict_load接口。

--- src/wrapper/python/opencc.py
+++ src/wrapper/python/opencc.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 from ctypes import *
 import sys
 
@@ -5,6 +8,7 @@ class opencc:
 	def __init__(self, convert_direction):
 		self.libopencc = cdll.LoadLibrary("libopencc.so")
 		self.od = self.libopencc.opencc_open(c_int(convert_direction))
+		self.libc = cdll.LoadLibrary("libc.so.6")
 	
 	def __del__(self):
 		self.libopencc.opencc_close(self.od)
@@ -13,7 +17,15 @@ class opencc:
 		text_c = c_char_p(text)
 		len_c = c_int(len(text))
 		retv_c = c_char_p(self.libopencc.opencc_convert_utf8(self.od, text_c, len_c))
-		return retv_c.value
+		str_buffer = retv_c.value
+		self.libc.free(retv_c);
+		return str_buffer
+	
+	def dict_load(self, filename, dicttype):
+		filename_c = c_char_p(filename)
+		dicttype_c = c_int(dicttype)
+		retv = c_int(self.libopencc.opencc_dict_load(self.od, filename_c, dicttype_c));
+		return retv
 
 if __name__ == "__main__":
 	converter = opencc(0)

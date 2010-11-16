9def410ea15987b484e0cb75b260aa965894de03
BYVoid <byvoid.kcp@gmail.com>
2010-11-16 19:18:59 +0800

    更新作者信息。

--- src/tools/opencc_dict.c
+++ src/tools/opencc_dict.c
@@ -1,7 +1,7 @@
 /*
 * Open Chinese Convert
 *
-* Copyright 2010 BYVoid <byvoid1@gmail.com>
+* Copyright 2010 BYVoid <byvoid.kcp@gmail.com>
 *
 * Licensed under the Apache License, Version 2.0 (the "License");
 * you may not use this file except in compliance with the License.

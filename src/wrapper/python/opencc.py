2a4384568e06343e5f90f7776e24c7db15271ea9
Kefu Chai <tchaikov@gmail.com>
2010-12-15 00:57:34 +0800

    refactor the python wrapper
    * don't specify the fname of lib to load (Mac and Win32 don't have 'so')
    * handle the convertion failure
    * load dict in python wrapper sample

--- src/wrapper/python/opencc.py
+++ src/wrapper/python/opencc.py
@@ -1,35 +1,72 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from ctypes import *
+from ctypes import cast, cdll, c_char_p, c_int, c_size_t, c_void_p
+from ctypes.util import find_library
 import sys
 
-class opencc:
-	def __init__(self, convert_direction):
-		self.libopencc = cdll.LoadLibrary("libopencc.so")
-		self.od = self.libopencc.opencc_open(c_int(convert_direction))
-		self.libc = cdll.LoadLibrary("libc.so.6")
+class ConvertError(Exception):
+	pass
+
+class DictType:
+	TEXT,DATRIE = 0,1
+
+class OpenCC:
+
+	def __init__(self, config=None, verbose=True):
+		self.libopencc = cdll.LoadLibrary(find_library('opencc'))
+		self.libopencc.opencc_open.restype = c_void_p
+		self.libopencc.opencc_convert_utf8.argtypes = [c_void_p, c_char_p, c_size_t]
+		# for checking for the returned '-1' pointer in case opencc_convert() fails.
+		# c_char_p always tries to convert the returned (char *) to a Python string,
+		self.libopencc.opencc_convert_utf8.restype = c_void_p
+		self.libopencc.opencc_close.argtypes = [c_void_p]
+		self.libopencc.opencc_perror.argtypes = [c_char_p]
+		self.libopencc.opencc_dict_load.argtypes = [c_void_p, c_char_p, c_int]
+
+		self.libc = cdll.LoadLibrary(find_library('c'))
+		self.libc.free.argtypes = [c_void_p]
+
+		self.config = config
+		self.verbose = verbose
+		self.od = None
 	
-	def __del__(self):
+	def __enter__(self):
+		if self.config is None:
+			self.od = self.libopencc.opencc_open(0)
+		else:
+			self.od = self.libopencc.opencc_open(c_char_p(self.config))
+		return self
+
+	def __exit__(self, type, value, traceback):
 		self.libopencc.opencc_close(self.od)
+		self.od = None
+
+	def __perror(self, message):
+		if self.verbose:
+			self.libopencc.opencc_perror(message)
 	
 	def convert(self, text):
-		text_c = c_char_p(text)
-		len_c = c_int(len(text))
-		retv_c = c_char_p(self.libopencc.opencc_convert_utf8(self.od, text_c, len_c))
+		retv_c = self.libopencc.opencc_convert_utf8(self.od, text, len(text))
+		if retv_c == -1:
+			self.__perror('OpenCC error:')
+			raise ConvertError()
+		retv_c = cast(retv_c, c_char_p)
 		str_buffer = retv_c.value
 		self.libc.free(retv_c);
 		return str_buffer
 	
 	def dict_load(self, filename, dicttype):
-		filename_c = c_char_p(filename)
-		dicttype_c = c_int(dicttype)
-		retv = c_int(self.libopencc.opencc_dict_load(self.od, filename_c, dicttype_c));
+		retv = self.libopencc.opencc_dict_load(self.od, filename, dicttype)
+		if retv == -1:
+			self.__perror('OpenCC error:')
 		return retv
 
 if __name__ == "__main__":
-	converter = opencc(0)
-	fp = sys.stdin
-	text = fp.read()
-	fp.close()
-	print converter.convert(text)
+	with sys.stdin as fp:
+		text = fp.read()
+	with OpenCC() as converter:
+		for path in ['simp_to_trad_characters.ocd',
+			     'simp_to_trad_phrases.ocd']:
+			converter.dict_load(path, DictType.DATRIE)
+		print converter.convert(text)

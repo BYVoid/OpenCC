f063e4807abbc5e7b78516be1014ba63690691a3
BYVoid <byvoid@byvoid.com>
2013-04-14 19:12:52 +0800

    Add build documentation option in CMakeLists.txt

--- debug.sh
+++ debug.sh
@@ -2,6 +2,7 @@ mkdir -p debug \
 && cd debug \
 && cmake \
 	-D ENABLE_GETTEXT:BOOL=OFF \
+	-D BUILD_DOCUMENTATION:BOOL=ON \
 	-DCMAKE_BUILD_TYPE=Debug \
 	-DCMAKE_INSTALL_PREFIX=`pwd`/root \
 	.. \
--- release.sh
+++ release.sh
@@ -1,10 +1,11 @@
 mkdir -p release \
 && cd release \
 && cmake \
-        -D ENABLE_GETTEXT:BOOL=ON \
-        -DCMAKE_BUILD_TYPE=Release \
-        -DCMAKE_INSTALL_PREFIX=/usr \
-        .. \
+	-D ENABLE_GETTEXT:BOOL=ON \
+	-D BUILD_DOCUMENTATION:BOOL=ON \
+	-DCMAKE_BUILD_TYPE=Release \
+	-DCMAKE_INSTALL_PREFIX=/usr \
+	.. \
 && make \
 && make test \
 && make package_source

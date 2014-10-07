da130bec1a93ce26b57d5d3e04a79b5d6640b21d
Carbo Kuo <byvoid@byvoid.com>
2014-10-07 13:53:48 +0200

    Optimize MaxMatchSegmentation::Segment
    Use pointer instead of vector<string>

--- src/MaxMatchSegmentation.cpp
+++ src/MaxMatchSegmentation.cpp
@@ -23,24 +23,26 @@ using namespace opencc;
 
 vector<string> MaxMatchSegmentation::Segment(const string& text) const {
   vector<string> segments;
-  vector<string> buffer;
+  const char* segStart = text.c_str();
+  size_t segLength = 0;
   auto clearBuffer =
-    [&segments, &buffer]() {
-      if (buffer.size() > 0) {
-        segments.push_back(UTF8Util::Join(buffer));
-        buffer.clear();
+    [&segments, &segStart, &segLength]() {
+      if (segLength > 0) {
+        segments.push_back(UTF8Util::FromSubstr(segStart, segLength));
+        segLength = 0;
       }
     };
   for (const char* pstr = text.c_str(); *pstr != '\0';) {
-    Optional<const DictEntry*> matched = dict->MatchPrefix(pstr);
+    const Optional<const DictEntry*>& matched = dict->MatchPrefix(pstr);
     size_t matchedLength;
     if (matched.IsNull()) {
       matchedLength = UTF8Util::NextCharLength(pstr);
-      buffer.push_back(UTF8Util::FromSubstr(pstr, matchedLength));
+      segLength += matchedLength;
     } else {
       clearBuffer();
       matchedLength = matched.Get()->Key().length();
       segments.push_back(matched.Get()->Key());
+      segStart = pstr + matchedLength ;
     }
     pstr += matchedLength;
   }

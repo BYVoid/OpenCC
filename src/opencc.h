9873477abc8ba9640ffd47cb7986b1089ee17055
Carbo Kuo <byvoid@byvoid.com>
2014-11-09 00:11:48 +0100

    Separate SimpleConverter.hpp from opencc.h

--- src/opencc.h
+++ src/opencc.h
@@ -23,6 +23,7 @@
 
 #include <string>
 #include "Export.hpp"
+#include "SimpleConverter.hpp"
 
 extern "C" {
 #else
@@ -137,72 +138,7 @@ OPENCC_EXPORT void opencc_convert_utf8_free(char* str);
 OPENCC_EXPORT const char* opencc_error(void);
 
 #ifdef __cplusplus
-}
-
-/**
-* @defgroup opencc_simple_api OpenCC C++ Simple API
-*
-* Simple API in C++ language
-*/
-
-namespace opencc {
-/**
-* A high level converter
-* This interface does not require C++11 to compile.
-* @ingroup opencc_simple_api
-*/
-class OPENCC_EXPORT SimpleConverter {
-public:
-  /**
-  * Constructor of SimpleConverter
-  * @param configFileName File name of configuration.
-  */
-  SimpleConverter(const std::string& configFileName);
-
-  ~SimpleConverter();
-
-  /**
-  * Converts a text
-  * @param input Text to be converted.
-  */
-  std::string Convert(const std::string& input) const;
-
-  /**
-  * Converts a text
-  * @param input A C-Style string (terminated by '\0') to be converted.
-  */
-  std::string Convert(const char* input) const;
-
-  /**
-  * Converts a text
-  * @param input  A C-Style string limited by a given length to be converted.
-  * @param length Maximal length in byte of the input string.
-  */
-  std::string Convert(const char* input, size_t length) const;
-  
-  /**
-  * Converts a text and writes to an allocated buffer
-  * Please make sure the buffer has sufficent space.
-  * @param input  A C-Style string (terminated by '\0') to be converted.
-  * @param output Buffer to write the converted text.
-  * @return       Length of converted text.
-  */
-  size_t Convert(const char* input, char* output) const;
-  
-  /**
-  * Converts a text and writes to an allocated buffer
-  * Please make sure the buffer has sufficent space.
-  * @param input  A C-Style string limited by a given length to be converted.
-  * @param length Maximal length in byte of the input string.
-  * @param output Buffer to write the converted text.
-  * @return       Length of converted text.
-  */
-  size_t Convert(const char* input, size_t length, char* output) const;
-
-private:
-  const void* internalData;
-};
-}
+} // extern "C"
 #endif
 
 /**

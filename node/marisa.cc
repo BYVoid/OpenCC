081463c8e10fe3cac9683dc7c31a84ede34b539a
Carbo Kuo <byvoid@byvoid.com>
2020-05-09 17:23:16 +0900

    Change back the include sequence in marisa.cc.

--- node/marisa.cc
+++ node/marisa.cc
@@ -1,10 +1,13 @@
 // For faster build
+#include "marisa/trie.cc"
+
 #include "marisa/agent.cc"
-#include "marisa/grimoire/io/mapper.cc"
+
 #include "marisa/grimoire/io/reader.cc"
 #include "marisa/grimoire/io/writer.cc"
+
+#include "marisa/grimoire/io/mapper.cc"
 #include "marisa/grimoire/trie/louds-trie.cc"
 #include "marisa/grimoire/trie/tail.cc"
 #include "marisa/grimoire/vector/bit-vector.cc"
 #include "marisa/keyset.cc"
-#include "marisa/trie.cc"

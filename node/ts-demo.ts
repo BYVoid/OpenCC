9abd5853356849d3338756702005b0d15d9834f1
Carbo Kuo <byvoid@byvoid.com>
2020-05-14 23:44:48 +0900

    Add more type annotations in ts-demo.ts.

--- node/ts-demo.ts
+++ node/ts-demo.ts
@@ -1,9 +1,30 @@
+/**
+ * @file
+ * Example of Node.js API.
+ *
+ * @license
+ * Open Chinese Convert
+ *
+ * Copyright 2010-2020 BYVoid <byvoid@byvoid.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import { OpenCC } from './opencc';
 
-const converter = new OpenCC('s2t.json');
-
 async function main() {
-  const result = await converter.convertPromise('汉字');
+  const converter: OpenCC = new OpenCC('s2t.json');
+  const result: string = await converter.convertPromise('汉字');
   console.log(result);
 }
 

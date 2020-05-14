88189180d00b82d6b810bcb85d399adf58892fb5
Wang Ming Rui <mingrui.wang@C02CJTUYMD6M.lan>
2020-05-14 22:48:18 +0800

    add pybind11 to deps

---
+++ deps/pybind11-2.5.0/pybind11/__init__.py
@@ -0,0 +1,12 @@
+from ._version import version_info, __version__  # noqa: F401 imported but unused
+
+
+def get_include(user=False):
+    import os
+    d = os.path.dirname(__file__)
+    if os.path.exists(os.path.join(d, "include")):
+        # Package is installed
+        return os.path.join(d, "include")
+    else:
+        # Package is from a source directory
+        return os.path.join(os.path.dirname(d), "include")

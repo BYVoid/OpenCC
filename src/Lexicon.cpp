7a3019e6c6e98eb59d6d4f93a3ad0ac628b8d205
Carbo Kuo <byvoid@byvoid.com>
2020-05-16 21:39:10 +0900

    Move sort functions into Lexicon.cpp.

---
+++ src/Lexicon.cpp
@@ -0,0 +1,33 @@
+/*
+ * Open Chinese Convert
+ *
+ * Copyright 2020 Carbo Kuo <byvoid@byvoid.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <algorithm>
+
+#include "Lexicon.hpp"
+namespace opencc {
+
+void Lexicon::Sort() {
+  std::sort(entries.begin(), entries.end(), DictEntry::UPtrLessThan);
+}
+
+bool Lexicon::IsSorted() {
+  return std::is_sorted(entries.begin(), entries.end(),
+                        DictEntry::UPtrLessThan);
+}
+
+} // namespace opencc

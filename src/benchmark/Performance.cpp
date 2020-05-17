7d1aa3882d6d9ddc8cd78b6fa8d65e5ac36752c5
sgalal <32255369+chromezh@users.noreply.github.com>
2020-05-17 21:54:12 +0800

    Update tests and sort in alphabetical order

--- src/benchmark/Performance.cpp
+++ src/benchmark/Performance.cpp
@@ -44,16 +44,18 @@ static void BM_Initialization(benchmark::State& state,
     state.ResumeTiming();
   }
 }
+BENCHMARK_CAPTURE(BM_Initialization, hk2s, "hk2s");
+BENCHMARK_CAPTURE(BM_Initialization, hk2t, "hk2t");
+BENCHMARK_CAPTURE(BM_Initialization, jp2t, "jp2t");
+BENCHMARK_CAPTURE(BM_Initialization, s2hk, "s2hk");
 BENCHMARK_CAPTURE(BM_Initialization, s2t, "s2t");
-BENCHMARK_CAPTURE(BM_Initialization, t2s, "t2s");
 BENCHMARK_CAPTURE(BM_Initialization, s2tw, "s2tw");
 BENCHMARK_CAPTURE(BM_Initialization, s2twp, "s2twp");
+BENCHMARK_CAPTURE(BM_Initialization, t2jp, "t2jp");
+BENCHMARK_CAPTURE(BM_Initialization, t2s, "t2s");
 BENCHMARK_CAPTURE(BM_Initialization, tw2s, "tw2s");
 BENCHMARK_CAPTURE(BM_Initialization, tw2sp, "tw2sp");
-BENCHMARK_CAPTURE(BM_Initialization, s2hk, "s2hk");
-BENCHMARK_CAPTURE(BM_Initialization, hk2s, "hk2s");
-BENCHMARK_CAPTURE(BM_Initialization, t2jp, "t2jp");
-BENCHMARK_CAPTURE(BM_Initialization, jp2t, "jp2t");
+BENCHMARK_CAPTURE(BM_Initialization, tw2t, "tw2t");
 
 static void BM_Convert(benchmark::State& state) {
   const std::string config_name = "s2t";

32a1ec786bd20628d56e84fcbe9701aeb5d1eb90
mingruimingrui <mingruimingrui@hotmail.com>
2020-05-25 16:37:06 +0800

    Switch to using SimpleConverter

--- src/py_opencc.cpp
+++ src/py_opencc.cpp
@@ -1,49 +1,19 @@
 #include <pybind11/pybind11.h>
 #include "opencc.h"
 
-class PYOpenCC {
-private:
-    opencc_t converter;
-
-public:
-    std::string config;
-
-    PYOpenCC(const char* configFileName) {
-        config = configFileName;
-        converter = opencc_open(configFileName);
-        if (converter == (opencc_t) - 1) {
-            throw std::invalid_argument(opencc_error());
-        };
-    }
-
-    ~PYOpenCC() {
-        opencc_close(converter);
-    }
-
-    char* convert(const char* text, size_t length) {
-        return opencc_convert_utf8(converter, text, length);
-    };
-};
-
 namespace py = pybind11;
 
 PYBIND11_MODULE(opencc_clib, m) {
-    m.doc() = R"pbdoc(
-        OpenCC Pybind11 plugin
-        -----------------------
-        .. currentmodule:: opencc_clib
-        .. autosummary::
-           :toctree: _generate
-           PYOpenCC
-    )pbdoc";
+    py::class_<opencc::SimpleConverter>(m, "_OpenCC")
+        .def(py::init<const std::string&>())
+        .def(
+            "convert",
+            py::overload_cast<const char*, size_t>
+            (&opencc::SimpleConverter::Convert, py::const_)
+        );
 
-    py::class_<PYOpenCC>(m, "_OpenCC")
-        .def(py::init<const char*>())
-        .def_readonly("config", &PYOpenCC::config)
-        .def("convert", &PYOpenCC::convert);
-
-#ifdef VERSION_INFO
-    m.attr("__version__") = VERSION_INFO;
+#ifdef VERSION
+    m.attr("__version__") = VERSION;
 #else
     m.attr("__version__") = "dev";
 #endif

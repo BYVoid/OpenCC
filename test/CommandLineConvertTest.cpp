529ec75103adea1281f8768d9f608668edaf02ae
sgalal <32255369+chromezh@users.noreply.github.com>
2020-05-28 10:25:43 +0800

    Add hk configuration files

--- test/CommandLineConvertTest.cpp
+++ test/CommandLineConvertTest.cpp
@@ -105,8 +105,9 @@ TEST_P(ConfigurationTest, Convert) {
 }
 
 INSTANTIATE_TEST_CASE_P(CommandLine, ConfigurationTest,
-                        ::testing::Values("hk2t", "hks2s", "hks2t", "jp2t", "s2hk", "s2hks",
-                                          "s2t", "s2tw", "s2twp", "t2hk", "t2hks",
-                                          "t2jp", "t2s", "tw2s", "tw2sp", "tw2t"));
+                        ::testing::Values("hk2s", "hk2t", "hks2s", "hks2t", "jp2t",
+                                          "s2hk", "s2hks", "s2t", "s2tw", "s2twp",
+                                          "t2hk", "t2hks", "t2jp", "t2s", "tw2s",
+                                          "tw2sp", "tw2t"));
 
 } // namespace opencc

e3d6994415de566b2f4b9f546112fbd367f588a6
mingruimingrui <mingruimingrui@hotmail.com>
2020-08-09 22:13:01 +0800

    Use absolute path to conda.bat in windows image

--- release-pypi-windows.cmd
+++ release-pypi-windows.cmd
@@ -1,6 +1,9 @@
 @echo off
 setlocal EnableDelayedExpansion
 
+REM Initialize conda
+C:\Miniconda/condabin/conda.bat init powershell
+
 SET VERSIONS=2.7 3.5 3.6 3.7 3.8
 SET SOURCEDIR=%cd%
 
@@ -10,9 +13,9 @@ for %%v in (%VERSIONS%) do (
 
     REM Create and activate environment
     cd %ROOT_DIR%
-    CALL conda create -y -n py%%v python=%%v
+    CALL C:\Miniconda/condabin/conda.bat create -y -n py%%v python=%%v
     if !ERRORLEVEL! NEQ 0 (EXIT !ERRORLEVEL!)
-    CALL conda activate py%%v
+    CALL C:\Miniconda/condabin/conda.bat activate py%%v
     if !ERRORLEVEL! NEQ 0 (EXIT !ERRORLEVEL!)
     pip install --no-cache-dir setuptools wheel pytest
     if !ERRORLEVEL! NEQ 0 (EXIT !ERRORLEVEL!)
@@ -22,11 +25,11 @@ for %%v in (%VERSIONS%) do (
     if !ERRORLEVEL! NEQ 0 (EXIT !ERRORLEVEL!)
 
     REM Cleanup
-    CALL conda deactivate
+    CALL C:\Miniconda/condabin/conda.bat deactivate
     rmdir /S /Q build python\opencc\clib OpenCC.egg-info
 )
 
 REM Upload to PyPI
-REM conda activate py3.8
+REM C:\Miniconda/condabin/conda.bat activate py3.8
 REM python -m pip install twine
 REM python -m twine upload dist/*

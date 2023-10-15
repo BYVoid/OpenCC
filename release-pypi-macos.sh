973bb19a30a69084b599a96ed0c0ef060d18336b
Anton Melser <anton@melser.org>
2023-10-15 11:18:03 +0800

    Add Python 3.12 support and remove EOL 3.7
    Signed-off-by: Anton Melser <anton.melser@outlook.com>

--- release-pypi-macos.sh
+++ release-pypi-macos.sh
@@ -12,7 +12,7 @@ bash ${MINICONDA_FILENAME} -b -f -p $HOME/miniconda3
 export PATH=$HOME/miniconda3/bin:$PATH
 eval "$(conda shell.bash hook)"
 
-for VERSION in 3.7 3.8 3.9 3.10 3.11; do
+for VERSION in 3.8 3.9 3.10 3.11 3.12; do
     # Create and activate environment
     conda config --add channels conda-forge
     conda config --set channel_priority strict

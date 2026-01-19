441d74c35eee0f27c6aadd8464a1b38b506511a5
frankslin <frankslin@users.noreply.github.com>
2026-01-19 13:30:53 -0800

    ci: upload artifacts and cancel stale runs (#1022)

--- test.cmd
+++ test.cmd
@@ -1,4 +1,7 @@
 cmake -S. -Bbuild -DCMAKE_INSTALL_PREFIX:PATH=. -DENABLE_GTEST:BOOL=ON -DCMAKE_BUILD_TYPE=Debug
 cmake --build build --config Debug --target install
 cd build
-ctest --verbose -C Debug
+ctest --verbose -C Debug > ctest.log 2>&1
+set TEST_ERROR=%ERRORLEVEL%
+type ctest.log
+exit /b %TEST_ERROR%
